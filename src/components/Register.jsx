ae4df2246410f80efd4eb05eb20ccce49f4f5da3
Smita-2004 <smita23tiwari.007@gmail.com>
2025-07-10 16:03:51 +0530

    updated

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -9,7 +9,7 @@ export default function Register() {
         e.preventDefault();
         try {
             // const url = "http://localhost:8080/api/users/register";
-            const url = "https://cafe-backend-wheat.vercel.app//api/users/register";
+            const url = "https://cafe-backend-wheat.vercel.app/api/users/register";
             const result = await axios.post(url, user);
             setError("Data saved successfully");
         } catch (err) {
