bbae34e7ce964341290711d2cad8665469ffacc3
Smita-2004 <smita23tiwari.007@gmail.com>
2025-07-16 15:29:54 +0530

    updated

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,71 +1,7 @@
-// import { useState } from "react";
-// import axios from "axios";
-
-// export default function Register() {
-//     const [user, setUser] = useState({});
-//     const [error, setError] = useState();
-//     const API = import.meta.env.VITE_API_URL
-
-//     const handleSubmit = async (e) => {
-//         e.preventDefault();
-//         try {
-//             const url = `${API_URL}/api/users/register`;
-//             // const url = "https://cafe-backend-wheat.vercel.app/api/users/register";
-//             const result = await axios.post(url, user);
-//             setError("Data saved successfully");
-//         } catch (err) {
-//             console.log(err);
-//             setError("Something went wrong");
-//         }
-//     };
-
-//     return (
-//         <div>
-//             <h2>Registration Form</h2>
-//             <p >{error}</p>
-//             <form onSubmit={handleSubmit}>
-//                 <p>
-//                     <input
-//                         type="text"
-//                         placeholder="FirstName"
-//                         onChange={(e) => setUser({ ...user, firstName: e.target.value })}
-//                     />
-//                 </p>
-//                 <p>
-//                     <input
-//                         type="text"
-//                         placeholder="LastName"
-//                         onChange={(e) => setUser({ ...user, lastName: e.target.value })}
-//                     />
-//                 </p>
-//                 <p>
-//                     <input
-//                         type="text"
-//                         placeholder="Email Address"
-//                         onChange={(e) => setUser({ ...user, email: e.target.value })}
-//                     />
-//                 </p>
-//                 <p>
-//                     <input
-//                         type="password"
-//                         placeholder="New Password"
-//                         onChange={(e) => setUser({ ...user, password: e.target.value })}
-//                     />
-//                 </p>
-//                 <button type="submit">Submit</button>
-//             </form>
-
-//         </div>
-//     );
-// }
-
-
-
-
-
+import "./Register.css";
+// import { useRef } from "react";
 import { useState } from "react";
-import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
 import axios from "axios";
 export default function Register() {
   const [user, setUser] = useState({});
@@ -74,8 +10,8 @@ export default function Register() {
   const API_URL = import.meta.env.VITE_API_URL
   const handleSubmit = async () => {
     try {
-    const url = `${API_URL}/api/users/register`;
-     const result = await axios.post(url, user);
+      const url = `${API_URL}/api/users/register`;
+      const result = await axios.post(url, user);
       setError("Data saved successfully");
       Navigate("/login")
     } catch (err) {
@@ -84,44 +20,82 @@ export default function Register() {
     }
   };
   return (
-    <div>
-      <h2>Registration Form</h2>
-      {error}
-      <p>
-        <input
-          type="text"
-          placeholder="First Name"
-          onChange={(e) => setUser({ ...user, firstName: e.target.value })}
-        />
-      </p>
-      <p>
-        <input
-          type="text"
-          placeholder="Last Name"
-          onChange={(e) => setUser({ ...user, lastName: e.target.value })}
-        />
-      </p>
-      <p>
-        <input
-          type="text"
-          placeholder="Email Address"
-          onChange={(e) => setUser({ ...user, email: e.target.value })}
-        />
-      </p>
-      <p>
-        <input
-          type="password"
-          placeholder="New Password"
-          onChange={(e) => setUser({ ...user, password: e.target.value })}
-        />
-      </p>
-      <p>
-        <button onClick={handleSubmit}>Submit</button>
-      </p>
-      <hr />
-      <p>
-        Already a member?&nbsp;<Link to="/login">Login here...</Link>
-      </p>
+    <div className="App-Register-Row">
+      <div style={{ backgroundColor: "white" }}>
+        <h2>Registration Form</h2>
+        {error}
+        <p>
+          <input
+            type="text"
+            onChange={(e) => setUser({ ...user, firstName: e.target.value })}
+            placeholder="Enter First Name"
+          />
+        </p>
+        <p>
+          <input
+            type="text"
+            placeholder="Enter Last Name"
+            onChange={(e) => setUser({ ...user, lastName: e.target.value })}
+          />
+        </p>
+        <p>
+          <input
+            type="text"
+            placeholder="Enter Email Address"
+            onChange={(e) => setUser({ ...user, email: e.target.value })}
+          />
+        </p>
+        <p>
+          <input
+            type="password"
+            placeholder="Enter Password"
+            onChange={(e) => setUser({ ...user, password: e.target.value })}
+          />
+        </p>
+        <p>
+          <button onClick={handleSubmit}>Submit</button>
+        </p>
+        <hr />
+      <Link to="/login">Already a member? Login Here...</Link>
+      </div>
     </div>
   );
 }
+
+// export default function Register() {
+//   const firstName = useRef();
+//   const lastName = useRef();
+//   const email = useRef();
+//   const password = useRef();
+//   const handleSubmit = () => {
+//     const user = {
+//       firstName: firstName.current.value,
+//       lastName: lastName.current.value,
+//       email: email.current.value,
+//       password: password.current.value,
+//     };
+//     console.log(user);
+//   };
+//   return (
+//     <div className="App-Register-Row">
+//       <div style={{ backgroundColor: "white" }}>
+//         <h2>Registration Form</h2>
+//         <p>
+//           <input type="text" placeholder="Enter First Name" ref={firstName} />
+//         </p>
+//         <p>
+//           <input type="text" placeholder="Enter Last Name" ref={lastName} />
+//         </p>
+//         <p>
+//           <input type="text" placeholder="Enter Email Address" ref={email} />
+//         </p>
+//         <p>
+//           <input type="password" placeholder="Enter Password" ref={password} />
+//         </p>
+//         <p>
+//           <button onClick={handleSubmit}>Submit</button>
+//         </p>
+//       </div>
+//     </div>
+//   );
+// }
